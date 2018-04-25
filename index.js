8f4b6f1153dc2424c833622973e43ad9c9d77ac8
Caio Gondim <me@caiogondim.com>
2018-04-24 23:30:54 -0400

    Add observable interface

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
+const $$observable = require('symbol-observable').default
+const pipe = require('tubo')
 const typeFrom = require('type-from')
+
+/**
+ * Interoperability point for observable/reactive libraries.
+ * @returns {observable} A minimal observable of state changes.
+ * For more information, see the observable proposal:
+ * https://github.com/tc39/proposal-observable
+ */
+function implementObservableInterface (target) {
+  target[$$observable] = function () {
+    return ({
+      subscribe (observer) {
+        if (typeof observer !== 'object' || observer === null) {
+          throw new TypeError('Expected the observer to be an object.')
+        }
+
+        function observeState () {
+          if (observer.next) {
+            observer.next(target)
+          }
+        }
+
+        observeState()
+        const unsubscribe = target.subscribe(observeState)
+        return { unsubscribe }
+      },
+
+      [$$observable] () {
+        return this
+      }
+    })
+  }
+
+  return target
+}
 
 function implementSubscribeInterface (target) {
   let subscribers = []
@@ -25,10 +61,18 @@ function withSubscribe (target) {
     return function (...args) {
       // eslint-disable-next-line new-cap
       const instance = new target(...args)
-      return implementSubscribeInterface(instance)
+      return pipe(
+        instance,
+        implementSubscribeInterface,
+        implementObservableInterface
+      )
     }
   } else {
-    return implementSubscribeInterface(target)
+    return pipe(
+      target,
+      implementSubscribeInterface,
+      implementObservableInterface
+    )
   }
 }
 
--- index.test.js
+++ index.test.js
@@ -1,6 +1,8 @@
 /* eslint-env jest */
 
 const withSubscribe = require('./index')
+const Rx = require('rxjs')
+const $$observable = require('symbol-observable').default
 
 it('works with object', () => {
   const foo = withSubscribe({
@@ -93,4 +95,140 @@ it('returns unsubscribe function', () => {
   expect(foo.a).toEqual(5)
   expect(foo.b).toEqual(6)
   expect(subscribeCalls).toEqual(2)
+})
+
+// Tests adapted from https://github.com/reactjs/redux/blob/4e5f7ef3569e9ef6d02f7b3043b290dc093c853b/test/createStore.spec.js#L613
+describe('Symbol.observable interop point', () => {
+  it('should exist', () => {
+    const foo = withSubscribe({
+      a: 1,
+      b: 2
+    })
+    expect(typeof foo[$$observable]).toBe('function')
+  })
+
+  describe('returned value', () => {
+    it('should be subscribable', () => {
+      const foo = withSubscribe({
+        a: 1,
+        b: 2
+      })
+      const obs = foo[$$observable]()
+      expect(typeof obs.subscribe).toBe('function')
+    })
+
+    it('should throw a TypeError if an observer object is not supplied to subscribe', () => {
+      const foo = withSubscribe({
+        a: 1,
+        b: 2
+      })
+      const obs = foo[$$observable]()
+
+      expect(function () {
+        obs.subscribe()
+      }).toThrowError(new TypeError('Expected the observer to be an object.'))
+
+      expect(function () {
+        obs.subscribe(null)
+      }).toThrowError(new TypeError('Expected the observer to be an object.'))
+
+      expect(function () {
+        obs.subscribe(() => {})
+      }).toThrowError(new TypeError('Expected the observer to be an object.'))
+
+      expect(function () {
+        obs.subscribe({})
+      }).not.toThrow()
+    })
+
+    it('should return a subscription object when subscribed', () => {
+      const foo = withSubscribe({
+        a: 1,
+        b: 2
+      })
+      const obs = foo[$$observable]()
+      const sub = obs.subscribe({})
+      expect(typeof sub.unsubscribe).toBe('function')
+    })
+  })
+
+  it('should pass an integration test with no unsubscribe', () => {
+    const foo = withSubscribe({
+      a: 1,
+      b: 2
+    })
+    const observable = foo[$$observable]()
+    const results = []
+
+    observable.subscribe({
+      next (observed) {
+        results.push({
+          a: observed.a,
+          b: observed.b
+        })
+      }
+    })
+
+    foo.a = 3
+    foo.b = 4
+
+    expect(results).toEqual([
+      { a: 1, b: 2 },
+      { a: 3, b: 2 },
+      { a: 3, b: 4 }
+    ])
+  })
+
+  it('should pass an integration test with an unsubscribe', () => {
+    const foo = withSubscribe({
+      a: 1,
+      b: 2
+    })
+    const observable = foo[$$observable]()
+    const results = []
+
+    const sub = observable.subscribe({
+      next (observed) {
+        results.push({
+          a: observed.a,
+          b: observed.b
+        })
+      }
+    })
+
+    foo.a = 3
+    sub.unsubscribe()
+    foo.b = 4
+
+    expect(results).toEqual([
+      { a: 1, b: 2 },
+      { a: 3, b: 2 }
+    ])
+  })
+
+  it('should pass an integration test with a common library (RxJS)', () => {
+    const foo = withSubscribe({
+      a: 1,
+      b: 2
+    })
+    const observable = Rx.Observable.from(foo)
+    const results = []
+
+    const sub = observable
+      .map(snapshot => ({
+        fromRx: true,
+        a: snapshot.a,
+        b: snapshot.b
+      }))
+      .subscribe(snapshot => results.push(snapshot))
+
+    foo.a = 3
+    sub.unsubscribe()
+    foo.b = 4
+
+    expect(results).toEqual([
+      { a: 1, b: 2, fromRx: true },
+      { a: 3, b: 2, fromRx: true }
+    ])
+  })
 })
